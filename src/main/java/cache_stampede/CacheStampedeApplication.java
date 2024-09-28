16e8ffb7668318355df4666aeb85da970db6fa6f
Jung-YouHwan <yhwjd99@gmail.com>
2024-09-28 15:47:17 +0900

    feat : 초기 설정

---
+++ src/main/java/cache_stampede/CacheStampedeApplication.java
@@ -0,0 +1,13 @@
+package cache_stampede;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheStampedeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CacheStampedeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cache_stampede/CacheStampedeApplicationTests.java
@@ -0,0 +1,13 @@
+package cache_stampede;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheStampedeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
